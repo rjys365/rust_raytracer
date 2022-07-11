dca01e1a72dbd086c4d6de72a9fd8b7c5a20770d
rjys365 <rjys365@hotmail.com>
2022-07-11 22:28:19 +0800

    fix shadow acne

--- src/generators/demo3.rs
+++ src/generators/demo3.rs
@@ -13,7 +13,7 @@ use std::rc::Rc;
 
 fn ray_color(r:&Ray,world:&dyn Hittable,depth:i32,rng: &mut ThreadRng)->Color{
     if depth<=0 {return Color::default();}
-    if let Some(rec)=world.hit(r, 0.0, INFINITY){
+    if let Some(rec)=world.hit(r, 0.001, INFINITY){
         let target=rec.p+rec.normal+Vec3::random_in_unit_sphere(rng);
         return 0.5*ray_color(&Ray::from(&rec.p, &(target-rec.p)), world, depth-1, rng);
     }
